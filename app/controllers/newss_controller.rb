8c4041b1fd5359ae4d4926f3766febfd71db0bfb
kano1201 <kano.nakamura3@gmail.com>
2021-07-15 01:02:03 +0000

    finish_news

--- app/controllers/newss_controller.rb
+++ app/controllers/newss_controller.rb
@@ -19,7 +19,7 @@ class NewssController < ApplicationController
   end
 
   def destroy
-    @nwes = News.find(params[:id])
+    @news = News.find(params[:id])
     @news.destroy
     redirect_to newss_index_path
   end
@@ -27,8 +27,8 @@ class NewssController < ApplicationController
   private #一種の境界線、「ここから下はcontrollerの中でしか呼び出せません」
   #privateより後に定義されたメソッドは、アクションとして認識されなくなる
 
-  def news_params #list_paramsではフォームで入力されたデータを受け取る
-    params.require(:news).permit(:title, :body)
+  def news_params #news_paramsではフォームで入力されたデータを受け取る
+    params.require(:news).permit(:news_title, :news_body)
     #paramsはRailsで送られてきた値を受け取るためのメソッド
     #requireでデータのオブジェクト名(ここでは:news)を指定し、permitでキー（:title,:body）を指定
   end
